91e4bc655717d2d8c964131e530f310bac3f09ac
Clement Lin <cola30616@gmail.com>
2024-02-27 14:46:43 +0800

    處理Layout

--- FinalGroupMVCPrj/Program.cs
+++ FinalGroupMVCPrj/Program.cs
@@ -1,4 +1,5 @@
 using FinalGroupMVCPrj.Data;
+using FinalGroupMVCPrj.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,7 +10,9 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
+var LifeShareLearnConnectionString = builder.Configuration.GetConnectionString("LifeShareLearn") ?? throw new InvalidOperationException("Connection string ' LifeShareLearn' not found.");
+builder.Services.AddDbContext<LifeShareLearnContext>(options =>
+    options.UseSqlServer(LifeShareLearnConnectionString));
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
