5773bb342939034bbdb986020462844750760e37
saycheese0318 <a1020345122@gmail.com>
2024-02-29 11:52:07 +0800

    02/29 資展桌電

--- FinalGroupMVCPrj/Controllers/TeacherAdminController.cs
+++ FinalGroupMVCPrj/Controllers/TeacherAdminController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FinalGroupMVCPrj.Models;
+using FinalGroupMVCPrj.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Pkcs;
 
 namespace FinalGroupMVCPrj.Controllers
 {
@@ -15,9 +18,35 @@ namespace FinalGroupMVCPrj.Controllers
             return View();
         }
         //■ ==========================     東霖作業區      ==========================■
-
-
-
+        private readonly LifeShareLearnContext _lifeShareLearnContext;
+        public TeacherAdminController(LifeShareLearnContext lifeShareLearnContext) {
+            _lifeShareLearnContext = lifeShareLearnContext;
+        }
+        // GET: TeacherAdmin/L
+        //動作簡述：回傳老師基本資訊
+        [HttpGet]
+        public IActionResult BasicInfo( )
+        {
+            //取得所有db老師資料放到ViewModel
+            IEnumerable<Class> vBasicVMCollection = new List<Class>(
+                    _lifeShareLearnContext.TTeachers
+                    .Select(tr =>new Class {
+                        TeacherName = tr.FTeacherName,
+                        TeacherProfilePic = tr.FTeacherProfilePic,
+                        Introduction = tr.FIntroduction,
+                        ContactInfo = tr.FContactInfo,
+                        Note = tr.FNote,
+                    })
+                );
+            return View("Basicinfo", vBasicVMCollection);
+        }
+        // GET: TeacherAdmin/
+        //動作簡述：回傳老師相關圖片
+        [HttpGet]
+        public IActionResult RelatedPic()
+        {
+            return View();
+        }
         //■ ==========================     子謙作業區      ==========================■
 
     }
---
+++ FinalGroupMVCPrj/Models/ViewModels/Class.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalGroupMVCPrj.Models.ViewModels
+{
+    public class Class
+    {
+        [Display(Name = "名字")]
+        public string? TeacherName { get; set; }
+        //public DateTime JoinDatetime { get; set; }
+        [Display(Name = "廠商ID")]
+        public byte[]? TeacherProfilePic { get; set; }
+        [Display(Name = "關於我")]
+        public string? Introduction { get; set; }
+        [Display(Name = "聯絡方式")]
+        public string? ContactInfo { get; set; }
+        [Display(Name = "備註")]
+        public string? Note { get; set; }
+    }
+}
