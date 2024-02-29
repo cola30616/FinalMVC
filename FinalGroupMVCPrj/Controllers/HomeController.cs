580d937ac43a87567ab384f1d27ccb7e6177cd18
Clement Lin <cola30616@gmail.com>
2024-02-29 14:49:29 +0800

    動態生成UI中

--- FinalGroupMVCPrj/Controllers/HomeController.cs
+++ FinalGroupMVCPrj/Controllers/HomeController.cs
@@ -28,23 +28,28 @@ namespace FinalGroupMVCPrj.Controllers
         public async Task<IActionResult> Index()
         {
             var courseList = await _context.TLessonCourses
-                .Select(course => new LessonCourseVM
-                {
-                    lessonCourse = course,
-                    // 老師名稱
-                    teacherName = _context.TTeachers
-                        .Where(teacher => teacher.FTeacherId == course.FTeacherId)
-                        .Select(teacher => teacher.FTeacherName)
-                        .FirstOrDefault() ?? "找不到當前老師",
-                    // 科目名稱
-                    subjectName = _context.TCourseSubjects
-                        .Where(sub => sub.FSubjectId == course.FSubjectId)
-                        .Select (sub => sub.FSubjectName)
-                        .FirstOrDefault() ?? "找不到科目名稱"
-                })
-                .ToListAsync();
-
+            .Include(course => course.FTeacher) // 加載 Teacher 導航屬性
+            .Select(course => new LessonCourseVM
+            {
+                lessonCourse = course,
+                // 老師名稱
+                teacherName = _context.TTeachers
+                    .Where(teacher => teacher.FTeacherId == course.FTeacherId)
+                    .Select(teacher => teacher.FTeacherName)
+                    .FirstOrDefault() ?? "找不到當前老師",
+                // 科目名稱
+                subjectName = _context.TCourseSubjects
+                    .Where(sub => sub.FSubjectId == course.FSubjectId)
+                    .Select(sub => sub.FSubjectName)
+                    .FirstOrDefault() ?? "找不到科目名稱",
+                // 圖片數據
+                imageData = course.FPhoto,
+                // 新增老師
+                teacher = course.FTeacher // 將加載的 Teacher 導航屬性賦值給 ViewModel 的 teacher 屬性
+            })
+            .ToListAsync();
             return View(courseList);
+
         }
         public IActionResult Test()
         {
--- FinalGroupMVCPrj/Models/ViewModel/LessonCourseVM.cs
+++ FinalGroupMVCPrj/Models/ViewModel/LessonCourseVM.cs
@@ -6,6 +6,10 @@
 
         public string teacherName { get; set; } // 添加教師名稱屬性
 
-        public string subjectName {  get; set; }// 添加科目名稱屬性
+        public string subjectName {  get; set; }// 添加科目名稱屬性課程
+        public byte[] imageData { get; set; } 
+        
+        public TTeacher teacher { get; set; }
     }
+
 }
