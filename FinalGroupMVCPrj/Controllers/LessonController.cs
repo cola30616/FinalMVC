5f83766575600d3cbc0d1b442c91041fcfe82e20
Clement Lin <cola30616@gmail.com>
2024-02-29 11:26:44 +0800

    UI 更新

---
+++ FinalGroupMVCPrj/Controllers/LessonController.cs
@@ -0,0 +1,25 @@
+﻿using FinalGroupMVCPrj.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalGroupMVCPrj.Controllers
+{
+    public class LessonController : UserInfoController
+    {
+        private readonly LifeShareLearnContext _context;
+        public LessonController(LifeShareLearnContext context)
+        {
+            _context = context;
+        }
+        //■ ==========================     子謙作業區      ==========================■
+        // GET: LessonHistory/List
+        //動作簡述：回傳課程記錄清單的頁面
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        //■ ==========================     翊妏 作業區      ==========================■
+    }
+}
