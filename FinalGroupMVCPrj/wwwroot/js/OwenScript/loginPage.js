f4cc845f30c709ecceb3b7c76d73d959b8ceb447
wen200796 <wen200796@gmail.com>
2024-03-02 01:41:49 +0800

    帳號設定排版完成/家裡桌電

---
+++ FinalGroupMVCPrj/wwwroot/js/OwenScript/loginPage.js
@@ -0,0 +1,38 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const signupTogle = document.querySelectorAll(".signupTogle");
+    const loginTogle = document.querySelectorAll(".loginTogle");
+    const toggleToSignup = document.getElementById("toggleToSignup")
+    const toggleToLogin = document.getElementById("toggleToLogin")
+    const submitBtn = document.getElementById("submitBtn")
+    toggleToSignup.addEventListener("click", () => {
+        ToSignup();
+    });
+
+    toggleToLogin.addEventListener("click", () => {
+        ToLogin();
+    });
+
+
+
+    function ToSignup() {
+        signupTogle.forEach((element) => {
+            element.style.display = "block";
+        });
+        loginTogle.forEach((element) => {
+            console.log("hi2");
+            element.style.display = "none";
+        });
+        submitBtn.innerHTML = "註冊"
+    };
+
+    function ToLogin() {
+        signupTogle.forEach((element) => {
+            element.style.display = "none";
+        });
+        loginTogle.forEach((element) => {
+            element.style.display = "block";
+        });
+        submitBtn.innerHTML = "登入"
+    };
+
+});
