f2c512cd0bbe0a877dbb81647ab336d9fc35123e
文唐 王 <tom1999112@gmail.com>
2024-03-03 12:19:54 +0800

    Invite

---
+++ FinalGroupMVCPrj/Controllers/PortfolioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalGroupMVCPrj.Controllers
+{
+	public class PortfolioController : UserInfoController
+	{
+		public IActionResult List()
+		{
+			
+			return View();
+		}
+	}
+}
