d6512eb7df67e925a2b264fc915e6143e7beaba4
wen200796 <wen200796@gmail.com>
2024-03-06 11:07:18 +0800

    update

--- FinalGroupMVCPrj/Controllers/LessonController.cs
+++ FinalGroupMVCPrj/Controllers/LessonController.cs
@@ -1,10 +1,12 @@
 ﻿using FinalGroupMVCPrj.Models;
 using FinalGroupMVCPrj.Models.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinalGroupMVCPrj.Controllers
 {
+    [AllowAnonymous]
     public class LessonController : UserInfoController
     {
         private readonly LifeShareLearnContext _context;
