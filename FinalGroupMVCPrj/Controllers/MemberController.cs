7037467736413175ee40b3d9adf3b36db99933d7
wen200796 <wen200796@gmail.com>
2024-03-07 01:34:00 +0800

    帳號設定畫面串接中/家裡桌電

--- FinalGroupMVCPrj/Controllers/MemberController.cs
+++ FinalGroupMVCPrj/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 ﻿using FinalGroupMVCPrj.Models;
+using FinalGroupMVCPrj.Models.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -18,12 +19,32 @@ namespace FinalGroupMVCPrj.Controllers
         [HttpGet]
         public IActionResult Setting()
         {
-
-            TMember? member = _context.TMembers.FirstOrDefault(m => m.FMemberId == GetCurrentMemberId());
-            
-            return View("OSetting", member);
+            int memberId = GetCurrentMemberId();
+            if (memberId == 0) { return Content("系統異常"); };
+            var dbMember = _context.TMembers.Include(m=>m.TMemberCitiesLists).Include(m=>m.TMemberWishFields).FirstOrDefault(m => m.FMemberId == memberId);
+            MemberDetailViewModel mdVm = new MemberDetailViewModel
+            {
+                Member = dbMember,
+                Cities = dbMember?.TMemberCitiesLists.Select(c=>c.FCityId),
+                WishFields = dbMember?.TMemberWishFields.Select(w => w.FFieldId),
+            };
+            ViewBag.JobSelectList = MemberDetailViewModel.GetJobSelectList();
+            return View("OSetting", mdVm);
         }
 
+        public IActionResult Setting2()
+        {
+            int memberId = GetCurrentMemberId();
+            if (memberId == 0) { return Content("系統異常"); };
+            var dbMember = _context.TMembers.Include(m => m.TMemberCitiesLists).Include(m => m.TMemberWishFields).FirstOrDefault(m => m.FMemberId == memberId);
+            MemberDetailViewModel mdVm = new MemberDetailViewModel
+            {
+                Member = dbMember,
+                Cities = dbMember?.TMemberCitiesLists.Select(c => c.FCityId),
+                WishFields = dbMember?.TMemberWishFields.Select(w => w.FFieldId),
+            };
+            return Content(mdVm?.Cities?.Skip(1).First().ToString());
+        }
         // GET: Member/Fav
         //動作簡述：回傳設定會員資訊的頁面
         [HttpGet]
