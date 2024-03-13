8f71c715514aaec38448b034b593f04343d36de8
wen200796 <wen200796@gmail.com>
2024-03-14 00:23:25 +0800

    後台會員單筆資料顯示開發中/家裡桌電

--- FinalGroupMVCPrj/Controllers/AdminMemberController.cs
+++ FinalGroupMVCPrj/Controllers/AdminMemberController.cs
@@ -3,6 +3,7 @@ using FinalGroupMVCPrj.Models.DTO;
 using FinalGroupMVCPrj.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinalGroupMVCPrj.Controllers
@@ -49,9 +50,50 @@ namespace FinalGroupMVCPrj.Controllers
                 q = q.Where(l => l.FLoginDateTime <= lastDate);
             }
 
-            var   loginHistory =  q.Skip(toSkip).Take(5).ToList();
+            var   loginHistory =  q.OrderByDescending(m=>m.FLoginDateTime).Skip(toSkip).Take(5).ToList();
 
             return Json(loginHistory);
+        }
+        // GET: AdminMember/Detail/1
+        //動作簡述：回傳會員詳細資訊 AdminMemberDTO
+        [HttpGet]
+        public IActionResult Detail(int id)
+        {
+            int memberId = id;
+            if (memberId == 0) { return BadRequest("沒有指定會員"); };
+            var dbMember = _context.TMembers.Include(m => m.TMemberCitiesLists).Include(m => m.TMemberWishFields).FirstOrDefault(m => m.FMemberId == memberId);
+            if (dbMember == null) { return StatusCode(500,"資料庫系統異常"); };
+            try
+            {
+                AdminMemberDTO mdVm = new AdminMemberDTO
+                {
+                    MemberId = dbMember.FMemberId,
+                    RealName = dbMember.FRealName,
+                    ShowName = dbMember.FShowName,
+                    Email = dbMember.FEmail,
+                    EmailConfirmed = dbMember.FEmailVerification?"已驗證":"未驗證",
+                    Phone = dbMember.FPhone,
+                    GetCampInfo = dbMember.FGetCampaignInfo?"是":"否",
+                    Birth = dbMember.FBirthDate?.ToString("yyyy/MM/dd"),
+                    Gender = dbMember.FGender == true ? "男" : (dbMember.FGender == false ? "女" : "其他／不便透露"),
+                    Job = dbMember.FJob,
+                    Education = dbMember.FEducation,
+                    Note = string.IsNullOrEmpty(dbMember.FNote)?"未連結任何帳號":"LINE" ,
+                    Cities =
+                    dbMember.TMemberCitiesLists.Join(
+                        _context.TCities, m => m.FCityId, c => c.FCityId,
+                        (m, c) => c.FCityName),
+                    WishFields =
+                                        dbMember.TMemberWishFields.Join(
+                        _context.TCourseFields, m => m.FFieldId, f => f.FFieldId,
+                        (m, f) => f.FFieldName)
+                };
+            return Ok(mdVm );
+            }catch (Exception ex)
+            {
+                return StatusCode(500, "系統異常："+ex);
+            }
+
         }
     }
 }
---
+++ FinalGroupMVCPrj/Models/DTO/AdminMemberDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalGroupMVCPrj.Models.DTO
+{
+    public class AdminMemberDTO
+    {
+        public int MemberId { get; set; } = 0;
+        public string? RealName { get; set; } = "";
+        public string? ShowName { get; set; } = "";
+        public string? Email { get; set; } = "";
+        public string? EmailConfirmed { get; set; } = "";
+        public string? GetCampInfo { get; set; } = "";
+        public string? Phone { get; set; } = "";
+        public string? Birth { get; set; } = "";
+        public string? Gender { get; set; } = "";
+        public string? Job { get; set; } = "";
+        public string? Education { get; set; } = "";
+        public string? Note { get; set; } = "";
+        public IEnumerable<string>? Cities { get; set; }
+        public IEnumerable<string>? WishFields { get; set; }
+    }
+}
