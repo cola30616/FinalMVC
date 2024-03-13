a17532c0d28418e882a5a4498d7a7e94c56bedf6
wen200796 <wen200796@gmail.com>
2024-03-13 11:59:21 +0800

    temp

--- FinalGroupMVCPrj/Models/ViewModels/OrderDetailViewModel.cs
+++ FinalGroupMVCPrj/Models/ViewModels/OrderDetailViewModel.cs
@@ -6,7 +6,19 @@ namespace FinalGroupMVCPrj.Models.ViewModels
     {
         public TOrder order { get; set; }
         public TOrderDetail orderDetail { get; set; }
+
+
+        public string FRealName { get; set; }
+        public string FPhone { get; set; }
+
+        public string FName { get; set; }
+
+        public string FDescription { get; set; }
+
+        public string FEmail { get; set; }
+
         public TLessonCourse lessonCourse { get; set; }
         public TMember member { get; set; }
+
     }
 }
