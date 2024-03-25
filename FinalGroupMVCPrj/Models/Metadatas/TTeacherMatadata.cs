ab06fc27188201468f2471474cf332551bfaf55f
saycheese0318 <a1020345122@gmail.com>
2024-03-25 09:17:01 +0800

    3/25 09:16a.m.

--- FinalGroupMVCPrj/Models/Metadatas/TTeacherMatadata.cs
+++ FinalGroupMVCPrj/Models/Metadatas/TTeacherMatadata.cs
@@ -9,9 +9,9 @@ namespace FinalGroupMVCPrj.Models.Metadatas
         public string? FTeacherName { get; set; }
         [Display(Name = "老師頭像")]
         public byte[]? FTeacherProfilePic { get; set; }
-        [Display(Name = "關於我")]
+        [Display(Name = "自我介紹")]
         public string? FIntroduction { get; set; }
-        [Display(Name = "聯絡方式")]
+        [Display(Name = "公開聯絡方式")]
         public string? FContactInfo { get; set; }
         [Display(Name = "備註")]
         public string? FNote { get; set; }
